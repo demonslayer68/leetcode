f7070c9c3f7b9e89863c84beda7e2a8d14daf343
demonslayer68 <demonslayer68@gmail.com>
2021-12-21 23:30:38 +0530

    faster solution for 5

--- 5_Longest_Palindromic_Substring.py
+++ 5_Longest_Palindromic_Substring.py
@@ -2,24 +2,24 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         length = len(s)
         pal = []
-        longest = 1
+        longest = 0
         output = [0, 0]
         for i in range(0, length - 1):
-            pal.append([i, i])
+            j = [i, i]
+            while j[0] > 0 and j[1] < length - 1 and s[j[0] - 1] == s[j[1] + 1]:
+                j[0] = j[0] - 1
+                j[1] = j[1] + 1
+            if j[1] - j[0] + 1 > longest:
+                longest = j[1] - j[0] + 1
+                output = [j[0], j[1]]
             if s[i] == s[i+1]:
-                pal.append([i, i+1])
-                longest = 2
-                output = [i, i + 1]
-        newpal = pal
-        while newpal:
-            pal = newpal.copy()
-            newpal = []
-            for j in pal:
-                if j[0] > 0 and j[1] < length - 1 and s[ j[0] - 1 ] == s[j[1] + 1]:
-                    newpal.append([j[0] - 1, j[1] + 1])
-                    if j[1] - j[0] + 3 > longest:
-                        longest = j[1] - j[0] + 3
-                        output = [j[0] - 1, j[1] + 1]
+                j = [i, i+1]
+                while j[0] > 0 and j[1] < length - 1 and s[j[0] - 1] == s[j[1] + 1]:
+                    j[0] = j[0] - 1
+                    j[1] = j[1] + 1
+                if j[1] - j[0] + 1 > longest:
+                    longest = j[1] - j[0] + 1
+                    output = [j[0], j[1]]
         return s[output[0]:output[1]+1:1]
 
 obj = Solution()
